3765ebf1ea7cf7a819606dd9606bb76916e1b34a
Thiagodsla <thiagod1192@gmail.com>
2021-10-25 17:35:03 -0300

    finalizando cracha

---
+++ nlw/main.js
@@ -0,0 +1,29 @@
+const linksSocialMidia = {
+    github: 'thiagodsla',
+    youtube: 'channel/UCrCt8IXmYlgrUmBBXN5MBow',
+    instagram: 'thiagodsla'
+};
+
+function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class');
+        li.children[0].href = `https://www.${social}.com/${linksSocialMidia[social]}`;
+    }
+}
+
+changeSocialMediaLinks();
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMidia.github}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name;
+            userBio.textContent = data.bio;
+            userLink.href = data.html_url;
+            userPhoto.src = data.avatar_url;
+            userLogin.textContent = data.login;
+        });
+}
+
+getGitHubProfileInfos();
